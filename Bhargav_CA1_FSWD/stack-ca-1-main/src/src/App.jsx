4d084c5f179979daccbdce8065b466de13daf2a8
unknown <bhargavraj1395@gmail.com>
2025-02-27 10:54:23 +0530

    CA_1!

---
+++ Bhargav_CA1_FSWD/stack-ca-1-main/src/src/App.jsx
@@ -0,0 +1,14 @@
+import TestimonialCard from './components/TestimonialCard'
+
+function App() {
+
+  return (
+    <>
+    <TestimonialCard name={"Arnab"} review={"good in DSA"}></TestimonialCard>
+    <TestimonialCard name={"Bhargav raj"} review={"not skilled in MERN Stack"}></TestimonialCard>
+    <TestimonialCard name={"Karan"} review={"skilled in MERN Stack"}></TestimonialCard>
+    </>
+  )
+}
+
+export default App
---
+++ Bhargav_CA1_FSWD/stack-ca-1-main/src/src/components/TestimonialCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TestimonialCard = ({name,review}) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h3>{review}</h3>
+    </div>
+  )
+}
+
+export default TestimonialCard
